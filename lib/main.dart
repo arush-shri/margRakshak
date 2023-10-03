7e73e4bd3dbf23c5fbcb045ef597a69296477397
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-10-03 19:01:09 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:marg_rakshak/view/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MaterialApp(home: LoginView(),));
+Future<void> main() async {
+  runApp(
+      const MaterialApp(home: LoginView())
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 
