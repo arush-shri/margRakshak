658989582b33f5b983e4b4ee1c847d861cd14470
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-11-13 15:46:14 +0530

    login page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:marg_rakshak/view/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:marg_rakshak/view/home.dart';
+import 'package:marg_rakshak/view/login.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  runApp(
-      const MaterialApp(home: LoginView())
-  );
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  runApp( const Main() );
 }
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context, designSize: const Size(450,800));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return const HomePage();
+          }
+          else{
+            return LoginView();
+          }
+        },
+      ),
+    );
+  }
+}
+
 
 
