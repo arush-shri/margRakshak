847779bb36e71c73de8735611741c5c8a8cd3cbd
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-11-14 15:41:55 +0530

    gmaps added

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +12,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -17,37 +25,42 @@ class _HomePageState extends State<HomePage> {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           home: Scaffold(
-            body: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    width: 450.w,
-                    height: 735.h,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30.w),
-                          bottomRight: Radius.circular(30.w)),
-                    ),
-                    child: Stack(
-                      children: [
-                        Positioned(child: Row(
-
-                        ))
-                      ],
-                    ),
+            body: Column(
+              children: [
+                Container(
+                  width: 450.w,
+                  height: 735.h,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30.w),
+                        bottomRight: Radius.circular(30.w)),
                   ),
-                  SizedBox(
-                    width: 450.w,
-                    height: 65.h,
-                    child: Stack(
-                      children: [
-                        Positioned(child: Row(
+                  child: Stack(
+                    children: [
+                      GoogleMap(
+                        onMapCreated: (GoogleMapController controller) {
+                          _controller.complete(controller);
+                        },
+                        mapType: MapType.satellite,
+                        initialCameraPosition: const CameraPosition(
+                          target: LatLng(37.43296265331129, -122.08832357078792),
+                          zoom: 11.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 450.w,
+                  height: 65.h,
+                  child: Stack(
+                    children: [
+                      Positioned(child: Row(
 
-                        ))
-                      ],
-                    ),
-                  )
-                ],
-              )
+                      ))
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         );
