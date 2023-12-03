f1335dd7b09c0299f01817e739cb7b2f3922e2a8
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-12-03 19:55:50 +0530

    fetched and showed danger

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -107,7 +107,6 @@ class _HomePageState extends State<HomePage> {
           markersList.add(
               Marker(
                 markerId: MarkerId(item["_id"].toString()),
-                flat: true,
                 position: pos,
                 infoWindow: InfoWindow(title: key),
               )
@@ -118,7 +117,6 @@ class _HomePageState extends State<HomePage> {
     await Future.delayed(const Duration(milliseconds: 2300));
     setState(() {
       dangerAhead.clear();
-      print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     });
   }
 
